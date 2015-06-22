010885b639ed867ca4984abb03d94b11f507d919
Caleb Kirksey <ckirksey3@gmail.com>
2015-06-21 23:11:11 -0400

    handle cors stuff

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ app.set('view engine', 'ejs');
 // Pull in all the Alexa apps
 require('require-dir')('./apps');
 
+app.all('*', function(req, res, next){
+  if (!req.get('Origin')) return next();
+  // use "*" here to accept any origin
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'PUT');
+  res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+  // res.set('Access-Control-Allow-Max-Age', 3600);
+  if ('OPTIONS' == req.method) return res.send(200);
+  next();
+});
+
 // Attach all the Alex apps to express
 alexa.bootstrap(app,'/');
 
