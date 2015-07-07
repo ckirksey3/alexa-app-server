4b626d0ef4122daac679768fe1cce446a345b3cd
Caleb Kirksey <ckirksey3@gmail.com>
2015-07-06 20:52:05 -0400

    try updated version of npm

--- server.js
+++ server.js
@@ -1,35 +1,49 @@
+
+// app.all('*', function(req, res, next){
+//   if (!req.get('Origin')) return next();
+//   // use "*" here to accept any origin
+//   res.set('Access-Control-Allow-Origin', '*');
+//   res.set('Access-Control-Allow-Methods', 'PUT');
+//   res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+//   // res.set('Access-Control-Allow-Max-Age', 3600);
+//   if ('OPTIONS' == req.method) return res.send(200);
+//   next();
+// });
+
+//NEW
+
 // Start up the server
 var express = require('express');
-var alexa = require('alexa-app');
+var alexa = require('../index.js');
 var bodyParser = require('body-parser');
 
 var app = express();
 var PORT = process.env.port || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.set('view engine', 'ejs');
+app.set('view engine','ejs');
 
-// Pull in all the Alexa apps
-require('require-dir')('./apps');
-
-app.all('*', function(req, res, next){
-  if (!req.get('Origin')) return next();
-  // use "*" here to accept any origin
-  res.set('Access-Control-Allow-Origin', '*');
-  res.set('Access-Control-Allow-Methods', 'PUT');
-  res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-  // res.set('Access-Control-Allow-Max-Age', 3600);
-  if ('OPTIONS' == req.method) return res.send(200);
-  next();
+var alexaApp = new alexa.app('test');
+alexaApp.launch(function(request,response) {
+	response.say("You launched the app!");
 });
+alexaApp.dictionary = {"names":["matt","joe","bob","bill","mary","jane","dawn"]};
+alexaApp.intent("nameIntent",
+	{
+		"slots":{"NAME":"LITERAL"}
+		,"utterances": [
+			"my {name is|name's} {names|NAME}"
+			,"set my name to {names|NAME}"
+		]
+	},
+	function(request,response) {
+		response.say("Success!");
+	}
+);
+alexaApp.express(app, "/echo/");
 
-// Attach all the Alex apps to express
-alexa.bootstrap(app,'/');
-
-// index page 
-app.get('/', function(req, res) {
-    res.render('test');
-});
+// Launch /echo/test in your browser with a GET request!
 
 app.listen(PORT);
 console.log("Listening on port "+PORT);
+
